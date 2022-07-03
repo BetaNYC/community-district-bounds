f44f1ea173668b70f77a3bbdbe413b3aa0fa19c6
Zhi Keng He <zhikhe@gmail.com>
2022-07-02 22:39:03 -0400

    fix markers

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
   import L from "leaflet";
   import "leaflet/dist/leaflet.css";
+  import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+  import markerIcon from 'leaflet/dist/images/marker-icon.png'
+  import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+  //fix for markers not showing up
+  delete L.Icon.Default.prototype._getIconUrl
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow
+  })
+  
   import { mapStore, districtsStore } from "./stores";
   import { onMount } from "svelte";
   import { formatDistrictPopup } from "./lib/format";
